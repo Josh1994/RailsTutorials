ced9cb198802c7cc39cf527ba9a2d6e11317f8d1
Josh <jjmoona0@gmail.com>
2017-08-05 23:27:44 +1200

    Completed 7.1.4

--- Chapter3/sample_app/app/controllers/users_controller.rb
+++ Chapter3/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    #debugger #uncomment if you want to debug the code during its current state
+              #and find what is causing an error.
+  end
+
   def new
   end
 end
