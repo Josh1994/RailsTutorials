71977d58930430eb16eafa128f584997b137bdbf
Josh <jjmoona0@gmail.com>
2017-11-06 21:00:33 +1300

    Sample following data and interface

--- Chapter3/sample_app/config/routes.rb
+++ Chapter3/sample_app/config/routes.rb
@@ -24,8 +24,15 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
 
+  resources :users do 
+    member do #arranges for the routes to respond to URLs containing the user id
+      get :following, :followers #/users/1/following and /users/1/followers
+    end
+  end
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
